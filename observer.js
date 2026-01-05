91e3e95b4df9c908c29ef0f5de613aa6222973a4
natfoster82 <nat.foster@gmail.com>
2026-01-05 16:45:00 -0700

    observer-event, observer-end, observer-response

--- observer.js
+++ observer.js
@@ -1,1 +1,108 @@
-import{SiteOSClient as e}from"https://caveon-public.s3.amazonaws.com/sos/script.min.js";const n=new e;function t(){n.emit("delivery_completed")}function o(e){const t={},o=["undefined","boolean","number","string"];for(const n in e){const i=e[n],d=typeof i;o.includes(d)||null===i?t[n]=i:t[n]=d}n.emit("event",t)}await n.propsReady(),n.on("event",(function(e){const n=new CustomEvent("observer",{detail:e});window.dispatchEvent(n)})),window.addEventListener("delivery_completed",t),window.addEventListener("click",o),window.addEventListener("contextmenu",o),window.addEventListener("keydown",o),window.addEventListener("keyup",o),window.addEventListener("focus",o),window.addEventListener("blur",o),window.addEventListener("resize",o),window.addEventListener("mousemove",function(e,n){let t=0;return function(...o){const i=Date.now();i-t>=n&&(t=i,e.apply(this,o))}}(o,100));
+import { SiteOSClient } from 'https://caveon-public.s3.amazonaws.com/sos/script.min.js?v=1'
+
+
+
+
+
+const client = new SiteOSClient()
+await client.propsReady()
+
+client.on('event', emitObserverEvent)
+
+
+
+
+
+function emitObserverEvent (event) {
+    const observerEvent = new CustomEvent('observer', { detail: event })
+
+    window.dispatchEvent(observerEvent)
+}
+
+
+
+
+
+function onCustomEvent (event) {
+    client.emit(event.type, event.detail)
+}
+
+
+
+
+
+function onEvent (event) {
+    const data = {}
+
+    const types = [ 'undefined', 'boolean', 'number', 'string' ]
+
+    for (const key in event) {
+        const value = event[key]
+
+        const type = typeof value
+
+        const allowed = types.includes(type)
+
+        if (allowed || value === null) {
+            data[key] = value
+
+            continue
+        }
+
+        data[key] = type
+    }
+
+    client.emit('event', data)
+}
+
+
+
+
+
+function throttle (cb, delay) {
+    let last = 0
+
+    return function (...args) {
+        const now = Date.now()
+
+        if (now - last >= delay) {
+            last = now
+
+            cb.apply(this, args)
+        }
+    }
+}
+
+
+
+
+
+function registerListeners () {
+    window.addEventListener('observer-event', onCustomEvent)
+
+    window.addEventListener('observer-end', onCustomEvent)
+
+    window.addEventListener('observer-response', onCustomEvent)
+
+    window.addEventListener('click', onEvent)
+
+    window.addEventListener('contextmenu', onEvent)
+
+    window.addEventListener('keydown', onEvent)
+
+    window.addEventListener('keyup', onEvent)
+
+    window.addEventListener('focus', onEvent)
+
+    window.addEventListener('blur', onEvent)
+
+    window.addEventListener('resize', throttle(onEvent, 100))
+
+    window.addEventListener('mousemove', throttle(onEvent, 100))
+}
+
+
+
+
+
+registerListeners()
